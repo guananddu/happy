c72d6f868ef5b4bb1b1eaac8b2610a2926bd2fe3
管伟 <guananddu@qq.com>
2016-11-15 11:13:15 +0800

    add timer2

--- test-nodejs/timers/setInterval.js
+++ test-nodejs/timers/setInterval.js
@@ -2,5 +2,10 @@ debugger
 var timer = setInterval( function () {
     debugger
     console.log( arguments );
-
 }, 2000, 'first', 123 );
+
+debugger
+var timer2 = setInterval( function () {
+    debugger
+    console.log( arguments );
+}, 4000, 'second', 456 );
