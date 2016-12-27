1ff587cd94925d52c8f8ec44dd47f1d9711a26c6
管伟 <guananddu@qq.com>
2016-12-27 18:48:15 +0800

    add buffer test codes

--- test-nodejs/buffer/main.js
+++ test-nodejs/buffer/main.js
@@ -28,3 +28,42 @@ console.log( 'buf5: ', buf5 );
 // Creates a Buffer containing UTF-8 bytes.
 const buf6 = Buffer.from( '这是一个 buffer 测试这是一个 buffer 测试', 'utf8' );
 console.log( 'buf6: ', buf6.toString() );
+
+
+// 测试 ArrayBuffer
+const arr = new Uint16Array( 2 );
+
+arr[ 0 ] = 5000;
+arr[ 1 ] = 4000;
+
+// Shares memory with `arr`
+const buf7 = Buffer.from( arr.buffer );
+
+// 10 进制的 5000 转成 16 进制即为 0x1388
+// 10 进制的 4000 转成 16 进制即为 0x0fa0
+// 实际排列为 低位在前，高位在后
+// Prints: <Buffer 88 13 a0 0f>
+console.log( buf7 );
+
+// Changing the original Uint16Array changes the Buffer also
+arr[ 1 ] = 6000;
+
+// Prints: <Buffer 88 13 70 17>
+console.log( buf7 );
+
+const ab = new ArrayBuffer( 10 );
+const buf8 = Buffer.from( ab, 0, 2 );
+
+// Prints: 2
+console.log( buf8.length );
+
+const buf9 = Buffer.from( 'buffer' );
+const buf10 = Buffer.from( buf9 );
+
+buf9[ 0 ] = 0x61;
+
+// Prints: auffer
+console.log( buf9.toString() );
+
+// Prints: buffer
+console.log( buf10.toString() );
