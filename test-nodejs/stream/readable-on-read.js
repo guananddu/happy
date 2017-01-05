cd3813ec6d38f4949ee19b9b1411bca2b81cfd3c
管伟 <guananddu@qq.com>
2017-01-05 14:26:22 +0800

    test readable pause &

--- test-nodejs/stream/readable-on-read.js
+++ test-nodejs/stream/readable-on-read.js
@@ -14,29 +14,29 @@ const filePath = path.resolve( __dirname, 'bigfile.txt' );
 
 //////////////
 
-// const readable = fs.createReadStream( filePath, {
-//     highWaterMark: 20000
-// } );
-//
-// readable.on( 'data', ( chunk ) => {
-//     console.log( `Received ${chunk.length} bytes of data.` );
-//     readable.pause();
-//     console.log( 'There will be no additional data for 1 second.' );
-//     setTimeout( () => {
-//         console.log( 'Now data will start flowing again.' );
-//         readable.resume();
-//     }, 1000 );
-// } );
-
-//////////////
-
 const readable = fs.createReadStream( filePath, {
     highWaterMark: 20000
 } );
 
-readable.on( 'readable', () => {
-    var chunk;
-    while ( null !== ( chunk = readable.read() ) ) {
-        console.log( `Received ${chunk.length} bytes of data.` );
-    }
+readable.on( 'data', ( chunk ) => {
+    console.log( `Received ${chunk.length} bytes of data.` );
+    readable.pause();
+    console.log( 'There will be no additional data for 1 second.' );
+    setTimeout( () => {
+        console.log( 'Now data will start flowing again.' );
+        readable.resume();
+    }, 1000 );
 } );
+
+//////////////
+
+// const readable = fs.createReadStream( filePath, {
+//     highWaterMark: 20000
+// } );
+//
+// readable.on( 'readable', () => {
+//     var chunk;
+//     while ( null !== ( chunk = readable.read() ) ) {
+//         console.log( `Received ${chunk.length} bytes of data.` );
+//     }
+// } );
