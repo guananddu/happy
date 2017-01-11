06685b82837ee247abc55ef2b2e8992c7c0490a3
管伟 <guananddu@qq.com>
2017-01-11 14:50:11 +0800

    add Stream.prototype.on break point

--- test-nodejs/stream/pipe.js
+++ test-nodejs/stream/pipe.js
@@ -5,6 +5,20 @@ const stream = require( 'stream' );
 const filePath = path.resolve( __dirname, 'bigfile.txt' );
 const outFilePath = path.resolve( __dirname, 'output.bigfile.txt' );
 
+// 为了在所有的事件处理器回调函数中断住
+const Stream = stream.Stream;
+
+let onOld = Stream.prototype.on;
+
+Stream.prototype.on = function ( ev, fn ) {
+    var that = this;
+    var innerFn = function () {
+        // debugger;
+        fn.apply( that, arguments );
+    }
+    onOld.call( that, ev, innerFn );
+};
+
 const readable = fs.createReadStream( filePath );
 const writable = fs.createWriteStream( outFilePath );
 
